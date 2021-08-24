c9063d5297fcf1995b9adcea4dd6ff7d45a66414
Bruh <bruh@bruh.com>
2021-08-25 00:12:21 +0300

    nz

--- player.zig
+++ player.zig
@@ -2,28 +2,30 @@
 const std = @import("std");
 
 const glob = @import("./glob.zig");
+const Settings = @import("./settings.zig").Settings;
 const Display = @import("./display.zig").Display;
 
 pub const Player = struct{
     phys: glob.Phys = undefined,
-    
+
     pub fn init(s: *@This(), aloc: *std.mem.Allocator) !void {
 
         var arr1: []const u8 = "   pussy";
         var arr2: []const u8 = "dest   royer";
 
-        var m0: []const u8 = "  0";
+        var m_1:[]const u8 = " ^ ^";
+        var m0: []const u8 = "  O";
         var m1: []const u8 = " /|\\";
         var m2: []const u8 = "  |";
         var m3: []const u8 = " / \\";
 
-        var ptrs: []*[]const u8 = ([_]*[]const u8{&m0, &m1, &m2, &m3})[0..];
+        var ptrs: []*[]const u8 = ([_]*[]const u8{&m_1, &m0, &m1, &m2, &m3})[0..];
 
         s.phys.model = try aloc.alloc([]u8, ptrs.len);
         errdefer aloc.free(s.phys.model);
 
         for(ptrs) |line, li| {
-        
+
             const model = try aloc.alloc(u8, line.len);
             errdefer aloc.free(model);
 
